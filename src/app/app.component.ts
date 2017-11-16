7a7dcfb93147366674325ebe809fcc82528513e8
dianmjs <32776894+dianmjs@users.noreply.github.com>
2017-11-16 16:26:20 -0500

    carpetas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Formularioclie } from "./componets/registroCliente";
 
 @Component({
   selector: 'app-root',
---
+++ src/app/componets/registroCliente.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+selector:"regisClie",
+templateUrl:"./app/vista/Formularioclie.html"
+})
+
+export class Formularioclie{
+    
+}
