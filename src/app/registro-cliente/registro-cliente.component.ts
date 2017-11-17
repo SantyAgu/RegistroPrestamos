95e4d8bca52ea57bde6779f96b58deab91a0edee
DIR\cristian.c.meneses <cristian.c.meneses@accenture.com>
2017-11-17 10:08:03 -0500

    modificacion registro

--- src/app/registro-cliente/registro-cliente.component.ts
+++ src/app/registro-cliente/registro-cliente.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro-cliente.component.css']
 })
 export class RegistroClienteComponent implements OnInit {
-
+  id:String;
   constructor() { }
 
   ngOnInit() {
