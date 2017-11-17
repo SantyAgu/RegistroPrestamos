fa6dbb71ab25635e8eb7d4cbca4d770b030e4d59
dianmjs <32776894+dianmjs@users.noreply.github.com>
2017-11-17 13:25:43 -0500

    data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import {DataBaseService} from './data-base.service';
+import {DataBaseService} from './services/data-base/data-base.service';
 
 import { AppComponent } from './app.component';
 import { RegistroClienteComponent } from './registro-cliente/registro-cliente.component';
