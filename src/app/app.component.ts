b0b404d626921840b991aaf9704f01d5ef45de26
DIR\cristian.c.meneses <cristian.c.meneses@accenture.com>
2017-11-22 13:57:41 -0500

    index

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { directiveDef } from '@angular/core/src/view/provider';
 import {DataBaseService} from './services/data-base/data-base.service';
 import { HttpClientModule } from '@angular/common/http';
 import {OnInit} from '@angular/core';
+import { RouterModule, Routes, Router } from '@angular/router';
 
 
 @Component({
@@ -14,12 +15,14 @@ import {OnInit} from '@angular/core';
 
 
 export class AppComponent implements OnInit {
-
-  constructor(private _dataBaseService: DataBaseService) {}
+  private router: Router;
   
 ngOnInit() {
-  
+  //this.router.navigate(["Aprobacion/" + this.cliente.id]);
 
+  }
+  show():boolean{
+    return false;
   }
 }
  
